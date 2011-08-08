7bd7108c9ae7d673836dfe774fced283bb81617d
Jon Stanford <jbirdvegas@gmail.com>
2011-08-08 14:06:32 -0400

    Allow user to configure wifi scan policy Change Ie9402412: Part 3 of 3
    Change-Id: Ifce2f34ef02ad93cdd984b5c519edf389ad262dc

--- wpa_supplicant/wpa_supplicant.c
+++ wpa_supplicant/wpa_supplicant.c
@@ -2014,7 +2014,8 @@ struct wpa_supplicant * wpa_supplicant_add_iface(struct wpa_global *global,
 #ifdef ANDROID
     char scan_prop[PROPERTY_VALUE_MAX];
     char *endp;
-    if (property_get("wifi.supplicant_scan_interval", scan_prop, "6") != 0) {
+    if ((property_get("persist.wifi_scan_interval", scan_prop, "5") != 0 ||
+        property_get("wifi.supplicant_scan_interval", scan_prop, "5") != 0) {
         wpa_s->scan_interval = (int)strtol(scan_prop, &endp, 0);
         if (endp == scan_prop) {
             wpa_s->scan_interval = 6;
